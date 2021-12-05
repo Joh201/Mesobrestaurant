54ab44fca1dd02e4586d0095a00251d7a128606a
Joh201 <johngt632@gmail.com>
2021-12-05 18:06:44 +0000

    Booking view

--- Mesobbooking/urls.py
+++ Mesobbooking/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mesob.views import home, booking_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('booklist/', booking_view, name ='book'),
 ]
--- mesob/views.py
+++ mesob/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def booking_view(request):
+    bookings = Book.objects.all().order_by('date_and_time')
+    context = {
+        'bookings': bookings
+    }
+    return render(request, 'booking_list.html', context)
+
+
+
+
