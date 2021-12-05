2e20830f96a310f1d2f47b232c7922b7787cdf0e
Joh201 <johngt632@gmail.com>
2021-12-05 12:32:23 +0000

    Add model and set up admin panel

--- mesob/models.py
+++ mesob/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Book(models.Model):
+    first_name = models.CharField(max_length=20, null=False, blank=False)
+    last_name = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    number_of_guests = models.IntegerField(null=False, blank=False)
+    date_and_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=timezone.now)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
