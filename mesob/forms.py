83e2a7bc22c727c79aebbb80a5cc29870f060781
Joh201 <johngt632@gmail.com>
2021-12-07 11:27:36 +0000

    Add datetime picker to forms.py

--- mesob/forms.py
+++ mesob/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Book
 
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
+
 
 class BookForm(forms.ModelForm):
+    date_and_time = forms.DateTimeField(widget=DateTimeInput, input_formats=["%Y-%m-%dT%H:%M", ])
     class Meta:
         model = Book
         fields = ['first_name', 'last_name', 'email', 'number_of_guests', 'date_and_time' ]
+
+
+# check_in = DateTimeField(required=True, input_formats=["%Y-%m-%dT%H:%M", ])
