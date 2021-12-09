91523cc487a47db301840a63c095303d72075a6e
Joh201 <johngt632@gmail.com>
2021-12-09 22:50:55 +0000

    Add list filter to admin panel

--- mesob/admin.py
+++ mesob/admin.py
@@ -3,8 +3,11 @@ from .models import Book
 
 # Register your models here.
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_displaly = ('first_name', 'last_name', 'email', 'number_of_guests', 'date_and_time')
-
+    # list_displaly = ('first_name', 'email', 'number_of_guests',
+    #                  'date_and_time')
+    list_filter = ('first_name', 'date_and_time')
+    search_fields = ['first_name', 'date_and_time', 'number_of_guests']
 
--- mesob/models.py
+++ mesob/models.py
@@ -8,7 +8,8 @@ class Book(models.Model):
     last_name = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     number_of_guests = models.IntegerField(null=False, blank=False)
-    date_and_time = models.DateTimeField(auto_now=False, auto_now_add=False, default=timezone.now)
+    date_and_time = models.DateTimeField(auto_now=False, auto_now_add=False,
+                                         default=timezone.now)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
