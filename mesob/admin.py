30ce2d733b6631e8edcdb0ea3a361bc5c9c60253
Joh201 <johngt632@gmail.com>
2021-12-11 16:20:53 +0000

    Update the list display of the admin

--- mesob/admin.py
+++ mesob/admin.py
@@ -5,6 +5,6 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     ''' Class that facilitates the management of the admin panel '''
-    list_displaly = ('last_name', 'email', 'number_of_guests')
+    list_display = ('last_name', 'email', 'number_of_guests')
     list_filter = ('first_name', 'date_and_time')
     search_fields = ['first_name', 'number_of_guests']
